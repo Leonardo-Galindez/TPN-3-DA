19912452881c38904a5ab99478ed34eda3603309
@HerWar <88455461+LeonardoGalindez@users.noreply.github.com>
2022-09-03 16:14:55 -0300

    cambios ejercicio 13

--- TPN-3-DA/src/Modulos.java
+++ TPN-3-DA/src/Modulos.java
@@ -3,7 +3,7 @@ public class Modulos {
     public static int CantPosColum(){
         Scanner sc= new Scanner(System.in);
         int PosColum;
-        System.out.println("Ingrese cantidad columnas de la Matriz");
+        System.out.println("Ingrese cantidad columnas");
         PosColum=sc.nextInt();
         return PosColum;
     }
@@ -11,7 +11,7 @@ public class Modulos {
     public static int CantPosFila(){
         Scanner sc= new Scanner(System.in);
         int PosFila;
-        System.out.println("Ingrese cantidad filas de la Matriz");
+        System.out.println("Ingrese cantidad filas");
         PosFila=sc.nextInt();
         return PosFila;
     
--- TPN-3-DA/src/TecladoIn.java
+++ TPN-3-DA/src/TecladoIn.java
@@ -1,5 +1,3 @@
-package Utiles;
-
 import java.io.*;
 import java.util.*;
 
